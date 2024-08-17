80acc5712de294c7a233e1e863ea3e4d0a47f3da
ulugbekbrat <168886327+ulugbekbrat@users.noreply.github.com>
2024-08-17 11:20:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+alert('Sizda hozir random sonlar chiqadi 🤑')
+let a = Math.floor (Math.random() * 100) ;
+
+alert(`Random orqali chiqqan son: ${lox =  Math.floor (Math.random() * 100) }`)
+
+
+
+alert('biz random sonni 2 ga kopaytirmochqi va bolmoqchimiz!')
+
+
+let kop = lox * 2
+let bol = lox / 2
+let qoldiq = lox % 3
+
+alert(`random orqali chiqqan son: ${lox}
+ edi biz uni 2 ga kopaytirib: ${kop} 
+ va shu sonni 2 ga bolib:  ${bol}
+ va shu sonni 3 ga bolgandagi qoldiq: ${qoldiq}  ` )
+
+  
+
+
+
+
